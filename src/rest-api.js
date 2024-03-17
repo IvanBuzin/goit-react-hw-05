0141b56093c38ead8282cbf3c59143dfd175ec19
IvanBuzin <ua.buzin@gmail.com>
2024-03-17 19:11:02 +0200

    s

---
+++ src/rest-api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.headers.common["Authorization"] = "";
+
+export const trendingMovie = async () => {
+  const response = await axios.get("/trending/movie/day");
+  return response.data.results;
+};
+
+export const searchMovie = async (query, page) => {
+  const response = await axios.get(`/search/movie?query=${query}&page=${page}`);
+  return response.data;
+};
+
+export const getMovieById = async (movieId) => {
+  const response = await axios.get(`/movie/${movieId}`);
+  return response.data;
+};
+
+export const getImagePath = async () => {
+  const response = await axios.get("/configuration");
+  return response.data.images;
+};
+
+export const getCredits = async (movieId) => {
+  const response = await axios.get(`/movie/${movieId}/credits`);
+
+  return response.data.cast;
+};
+
+export const getReviews = async (movieId) => {
+  const response = await axios.get(`/movie/${movieId}/reviews`);
+  return response.data.results;
+};
