36aafa36c9fe7ac4714d9ff7a132a75d0d6e63c3
IvanBuzin <ua.buzin@gmail.com>
2024-03-22 05:51:47 +0200

    sdfgh

--- src/rest-api.js
+++ src/rest-api.js
@@ -1,31 +1,48 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = "https://api.themoviedb.org/3";
-axios.defaults.headers.common["Authorization"] =
+const url = 'https://api.themoviedb.org/3';
+
+const options = {
+  params: { language: 'en-US' },
+  include_adult: false,
+  headers: {
+    Authorization:
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZGY3NDgxZTQ5N2NjNmIwYmM4YjY1YmNmMWVmY2EzZCIsInN1YiI6IjY1ZjcxZDNmZWY5ZDcyMDE3ZWQ1OWI4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ibmQkZm78mMh7I8Z7lBYklhxH8bLmfkW7xnBjxip6_I";
 
-export const fetchData = async () => {
-  const response = await axios.get("/trending/movie/day");
-  return response.data.results;
+ },
+  accept: 'application/json',
 };
 
-export const searchMovie = async (query, page) => {
-  const response = await axios.get(`/search/movie?query=${query}&page=${page}`);
+axios.defaults.baseURL = url;
+
+export async function fetchData() {
+  const response = await axios.get('trending/movie/day', options);
+
   return response.data;
-};
+}
 
-export const fetchMovieData = async (movieId) => {
-  const response = await axios.get(`/movie/${movieId}`);
+export async function fetchMovieData(movieId) {
+  const response = await axios.get(`movie/${movieId}`, options);
+
   return response.data;
-};
+}
 
-export const fetchMovieCast = async (movieId) => {
-  const response = await axios.get(`/movie/${movieId}/credits`);
+export async function fetchMovieReviews(movieId) {
+  const response = await axios.get(`movie/${movieId}/reviews`, options);
 
-  return response.data.cast;
-};
+  return response.data;
+}
 
-export const fetchMovieReviews = async (movieId) => {
-  const response = await axios.get(`/movie/${movieId}/reviews`);
-  return response.data.results;
-};
+export async function fetchMovieCast(movieId) {
+  const response = await axios.get(`movie/${movieId}/credits`, options);
+
+  return response.data;
+}
+
+export async function searchMovie(query) {
+  options.params.query = query;
+
+  const response = await axios.get('search/movie', options);
+
+  return response.data;
+}
