8fa2f8c8cd1eb54550aae19431d17d317c6bd76a
IvanBuzin <ua.buzin@gmail.com>
2024-03-22 06:45:10 +0200

    vft

--- src/components/MovieList/MavieList.jsx
+++ src/components/MovieList/MavieList.jsx
@@ -1,7 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
 import css from "./MovieList.module.css";
 
-const MovieList = ({ data }) => {
+const MovieList = ({ data, urlPath }) => {
+  const defaultImg =
+    "https://drive.google.com/file/d/1duL1VQXwqE_WgAS_279R3dJhwdF-9JZ3/view?usp=sharing";
   const location = useLocation();
 
   return (
@@ -9,6 +11,13 @@ const MovieList = ({ data }) => {
       {data.map((el) => {
         return (
           <li key={el.id} className={css.item}>
+            <img
+              className={css.img}
+              src={
+                data.poster_path ? `${urlPath}${data.poster_path}` : defaultImg
+              }
+              alt={data.title}
+            />
             <Link to={`/movies/${el.id}`} state={location}>
               {el.title}
             </Link>
